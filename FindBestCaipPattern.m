c0dda8c18ce042918cb8c8c54323357a81a43e58
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2013-07-29 16:36:38 +0200

    Started Repo.

---
+++ FindBestCaipPattern.m
@@ -0,0 +1,18 @@
+%% -1.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%                                 PROGRAM TO HELL AND BACK                           %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%     ENTSTANDEN DURCH DIE KUNST DES PROGRAMMIERENS DURCH KONSEQUENTES ANSTARREN     %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%     Find out the best CAIPIRINHA pattern by computing the average distance of      %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%                        measured to not measured k-space points.                    %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% 0. DEFINITIONS, PREPARATIONS
+
+cell_size = [4 4];      % e.g. undersampling of factor 2 in both directions, and to get more patterns double that size in both directions.
+
+
+%% 1. Create an image of a duerer image
+
+
+
+
