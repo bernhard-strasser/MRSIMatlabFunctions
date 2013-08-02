b4ec11c1f92fbab6ee20e67b86966d685d567575
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2013-08-02 13:48:09 +0200

    Removed Alins opengrappa, Added Version 1.0.

--- opengrappa_MRSI_1_0.m
+++ opengrappa_MRSI_1_0.m
@@ -1,17 +1,17 @@
-function [recon,ws]=opengrappa_time(sig, acs, af, type) 
+function [recon,ws]=opengrappa_MRSI_1_0(sig, acs, R, type) 
 
 % This code is written by Felix Breuer based on opengrappa by Mark Griswold 
 %
 % 22.06.2008 Felix Breuer (breuer@mr-bavaria.de)
 %
 %
-%  [recon,sigrecon,ws,ws_img,g] = opengrappa(sig,acs,af);
+%  [recon,sigrecon,ws,ws_img,g] = opengrappa(sig,acs,R);
 %
 %   This is a teaching version of the GRAPPA reconstruction code.
 %       
-%   IN:         sig                reduced data set         (#coils, Ky./af, Kx)
+%   IN:         sig                reduced data set         (#coils, Ky./R, Kx)
 %               acs                autocalibration lines    (#coils, #acs lines, Kx-acslines)    
-%               af                 Acceleration factor      (integer)
+%               R                  Acceleration factor      (integer)
 %
 %   OUT:        recon              Reconstructed images     (# coils, Ny, Nx)   
 %               ws                 Weight sets              
@@ -53,11 +53,23 @@ function [recon,ws]=opengrappa_time(sig, acs, af, type)
 %
 
 
-if strcmp(type, 'row') == 1
 
-    [nc,ny_red,nx, ns, nTime]=size(sig);
+
+
+
+
+
+
+
+
+
+
+
+if(strcmp(type, 'row') == 1)
+
+    [nc,ny,nx, ns, nTime]=size(sig);
+    ny_red = ny/R;
     [nc_acs,nyacs,nxacs]=size(acs);     %Get the size of both the input data and the ref data
-    ny = ny_red *af;
 
     if nc_acs~=nc
         disp('Error! The number of coils has to be the same for both inputs!')
@@ -71,7 +83,7 @@ if strcmp(type, 'row') == 1
 
 
     dx = floor(srcx/2);
-    dy = (srcy/2-1)*af; 
+    dy = (srcy/2-1)*R; 
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
@@ -84,15 +96,15 @@ if strcmp(type, 'row') == 1
 
     % Prepare source and target matrix
     % Number of source points -> nc*nsry*nsrcx
-    % Number of target points -> nc * (af-1)
-    % number of kernel repetitions in ACS data: (nyacs-(srcy-1)*af)*(nxacs-(srcx-1))
+    % Number of target points -> nc * (R-1)
+    % number of kernel repetitions in ACS data: (nyacs-(srcy-1)*R)*(nxacs-(srcx-1))
 
-    src = zeros(nc*srcy*srcx,(nyacs-(srcy-1)*af)*(nxacs-(srcx-1)));
-    trg = zeros(nc*(af-1),(nyacs-(srcy-1)*af)*(nxacs-(srcx-1)));
+    src = zeros(nc*srcy*srcx,(nyacs-(srcy-1)*R)*(nxacs-(srcx-1)));
+    trg = zeros(nc*(R-1),(nyacs-(srcy-1)*R)*(nxacs-(srcx-1)));
 
 
 
-    %   Simple example at af=2 and kernel size 5x4:            
+    %   Simple example at R=2 and kernel size 5x4:            
     %
     %                   -O-O-O-O-O-     1   
     %                   - - - - - -     2
@@ -112,14 +124,14 @@ if strcmp(type, 'row') == 1
     cnt = 0;                          %This is a very lazy counter. Could be done much faster. 
 
     for xind=dx+1:nxacs-dx,
-        for yind=1:nyacs-(srcy-1)*af,
+        for yind=1:nyacs-(srcy-1)*R,
             cnt=cnt+1;
 
             % These are the source points (#coils*srcy*srcx) 
-            src(:,cnt) = reshape(acs(:,yind:af:yind+(srcy-1)*af,xind-dx:xind+dx),nc*srcy*srcx,1);                   
+            src(:,cnt) = reshape(acs(:,yind:R:yind+(srcy-1)*R,xind-dx:xind+dx),nc*srcy*srcx,1);                   
 
-            % these are the taget points (#coils*(af-1))
-            trg(:,cnt) = reshape(acs(:,yind+1+dy:yind+dy+af-1,xind),nc*(af-1),1);
+            % these are the taget points (#coils*(R-1))
+            trg(:,cnt) = reshape(acs(:,yind+1+dy:yind+dy+R-1,xind),nc*(R-1),1);
 
         end
     end
@@ -130,7 +142,7 @@ if strcmp(type, 'row') == 1
     %ws=trg*pinv(src);  % find weights by fitting the source data to target data  
     ws=trg*pinv(squeeze(src(:,:,:, 1, 1)));
 
-    t=toc; fprintf('... %f sec \n',t)                                                          
+    fprintf('... %f sec \n',toc)                                                          
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
@@ -139,34 +151,53 @@ if strcmp(type, 'row') == 1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     tic; fprintf('Applyig weights ')
-    for i=1:nTime,
-        sigrecon = zeros(nc,ny+2*dy+1,nx+2*dx);                                                          % prepare matrix for convolution   
-
-        sigrecon(:,dy+1:af:ny+dy,dx+1:end-dx) = sig(:,:,:, 1, i);                                        % Write undersampled data into zero-padded matrix 
+    recon = zeros(size(sig));
+    for t=1:nTime
+        
+        sigrecon = zeros(nc,ny+2*dy+1,nx+2*dx);                                                       % prepare matrix for convolution   
+        sigrecon(:,dy+1:ny+dy,dx+1:end-dx) = sig(:,:,:, 1, t);                                        % Write undersampled data into zero-padded matrix 
 
         for xind = dx+1:nx+dx, 
-            for yind= 1:af:ny,
+            for yind= 1:R:ny,
 
-                src=reshape(sigrecon(:,yind:af:yind + (srcy-1)*af,xind-dx:xind+dx),nc*srcy*srcx,1);
+                src=reshape(sigrecon(:,yind:R:yind + (srcy-1)*R,xind-dx:xind+dx),nc*srcy*srcx,1);
 
-                sigrecon(:,yind+dy+1:yind+dy+af-1,xind)=reshape(ws*src,[nc (af-1)]);           %Apply weights to source points
+                sigrecon(:,yind+dy+1:yind+dy+R-1,xind)=reshape(ws*src,[nc (R-1)]);           %Apply weights to source points
 
             end
         end
-        sigrecon = sigrecon(:,dy+1:ny+dy,dx+1:nx+dx);                                           %Crop out the good data.
+        recon(:,:,:,:,t) = sigrecon(:,dy+1:ny+dy,dx+1:nx+dx);                                           %Crop out the good data.
 
-        recon_tmp=fftshift(fftshift(fft(fft(ifftshift(ifftshift(sigrecon,2),3),[],2),[],3),2),3);
-        recon(:, :, :, 1, i) = flipdim(recon_tmp,2);
     end
 
-    t=toc;
-    fprintf('... %f sec \n',t)
-else
-if strcmp(type, 'col') == 1
+    fprintf('... %f sec \n',toc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+elseif(strcmp(type, 'col') == 1)
   
-    [nc,ny,nx_red, ns, nTime]=size(sig);
+    [nc,ny,nx, ns, nTime]=size(sig);
+    nx_red = nx/R;
+    
     [nc_acs,nyacs,nxacs]=size(acs);     %Get the size of both the input data and the ref data
-    nx = nx_red *af;
 
     if nc_acs~=nc
         disp('Error! The number of coils has to be the same for both inputs!')
@@ -179,7 +210,7 @@ if strcmp(type, 'col') == 1
     srcy = 5;                         % should be even 
 
     dy = floor(srcy/2);
-    dx = (srcx/2-1)*af; 
+    dx = (srcx/2-1)*R; 
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
@@ -192,15 +223,15 @@ if strcmp(type, 'col') == 1
 
     % Prepare source and target matrix
     % Number of source points -> nc*nsry*nsrcx
-    % Number of target points -> nc * (af-1)
-    % number of kernel repetitions in ACS data: (nyacs-(srcy-1)*af)*(nxacs-(srcx-1))
+    % Number of target points -> nc * (R-1)
+    % number of kernel repetitions in ACS data: (nyacs-(srcy-1)*R)*(nxacs-(srcx-1))
     %x = nc*srcy*srcx;
-    %y = (nyacs-(srcy-1)*af)*(nxacs-(srcx-1));
+    %y = (nyacs-(srcy-1)*R)*(nxacs-(srcx-1));
 
-    src = zeros(nc*srcy*srcx,(nyacs-(srcy-1))*(nxacs-(srcx-1)*af));
-    trg = zeros(nc*(af-1),(nyacs-(srcy-1))*(nxacs-(srcx-1)*af));
+    src = zeros(nc*srcy*srcx,(nyacs-(srcy-1))*(nxacs-(srcx-1)*R));
+    trg = zeros(nc*(R-1),(nyacs-(srcy-1))*(nxacs-(srcx-1)*R));
 
-    %   Simple example at af=2 and kernel size 5x4:            
+    %   Simple example at R=2 and kernel size 5x4:            
     %
     %                   -O-O-O-O-O-     1   
     %                   - - - - - -     2
@@ -216,16 +247,16 @@ if strcmp(type, 'col') == 1
 
     cnt = 0;                          %This is a very lazy counter. Could be done much faster. 
 
-    for xind=1:nxacs-(srcx-1)*af,
+    for xind=1:nxacs-(srcx-1)*R,
         for yind=dy+1:nyacs-dy,
             cnt=cnt+1;
             % These are the source points (#coils*srcy*srcx) 
-            src(:,cnt) = reshape(acs(:,yind-dy:yind+dy, xind:af:xind+(srcx-1)*af), nc*srcy*srcx, 1);    
-            %src(:,cnt) = reshape(acs(:,yind:af:yind+(srcy-1)*af,xind-dx:xind+dx),nc*srcy*srcx,1);               
+            src(:,cnt) = reshape(acs(:,yind-dy:yind+dy, xind:R:xind+(srcx-1)*R), nc*srcy*srcx, 1);    
+            %src(:,cnt) = reshape(acs(:,yind:R:yind+(srcy-1)*R,xind-dx:xind+dx),nc*srcy*srcx,1);               
 
-            % these are the taget points (#coils*(af-1))
-            trg(:,cnt) = reshape(acs(:, yind, xind+1+dx:xind+dx+af-1),nc*(af-1),1);
-            %trg(:,cnt) = reshape(acs(:,yind+1+dy:yind+dy+af-1,xind),nc*(af-1),1);
+            % these are the taget points (#coils*(R-1))
+            trg(:,cnt) = reshape(acs(:, yind, xind+1+dx:xind+dx+R-1),nc*(R-1),1);
+            %trg(:,cnt) = reshape(acs(:,yind+1+dy:yind+dy+R-1,xind),nc*(R-1),1);
 
         end
     end
@@ -233,7 +264,7 @@ if strcmp(type, 'col') == 1
     %ws=trg*pinv(src);  % find weights by fitting the source data to target data  
     ws=trg*pinv(squeeze(src(:,:,:, 1, 1)));
 
-    t=toc; fprintf('... %f sec \n',t)                                                          
+    fprintf('... %f sec \n',toc)                                                          
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
@@ -242,30 +273,34 @@ if strcmp(type, 'col') == 1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     tic; fprintf('Applyig weights ')
-    for i = 1:nTime,
+    recon = zeros(size(sig));
+    for t = 1:nTime,
         sigrecon = zeros(nc,ny+2*dy,nx+2*dx+ 1);                                                         % prepare matrix for convolution     
-        sigrecon(:,dy+1:end-dy,dx+1:af:nx+dx) = sig(:,:,:, 1, i);                                        % Write undersampled data into zero-padded matrix 
-        %sigrecon(:,dy+1:af:ny+dy,dx+1:end-dx) = sig(:,:,:, 1, i);
+        sigrecon(:,dy+1:end-dy,dx+1:nx+dx) = sig(:,:,:, 1, t);                                           % Write undersampled data into zero-padded matrix 
+        %sigrecon(:,dy+1:R:ny+dy,dx+1:end-dx) = sig(:,:,:, 1, t);
 
-        for xind = 1:af:nx, 
+        for xind = 1:R:nx, 
             for yind = dy+1:ny+dy
-                src=reshape(sigrecon(:,yind-dy:yind+dy, xind:af:xind + (srcx-1)*af),nc*srcy*srcx,1);
-                %src=reshape(sigrecon(:,yind:af:yind + (srcy-1)*af,xind-dx:xind+dx),nc*srcy*srcx,1);
-                sigrecon(:,yind,xind+dx+1:xind+dx+af-1)=reshape(ws*src,[nc (af-1)]);           %Apply weights to source points
-                %sigrecon(:,yind+dy+1:yind+dy+af-1,xind)=reshape(ws*src,[nc (af-1)]);
+                src=reshape(sigrecon(:,yind-dy:yind+dy, xind:R:xind + (srcx-1)*R),nc*srcy*srcx,1);
+                %src=reshape(sigrecon(:,yind:R:yind + (srcy-1)*R,xind-dx:xind+dx),nc*srcy*srcx,1);
+                sigrecon(:,yind,xind+dx+1:xind+dx+R-1)=reshape(ws*src,[nc (R-1)]);           %Apply weights to source points
+                %sigrecon(:,yind+dy+1:yind+dy+R-1,xind)=reshape(ws*src,[nc (R-1)]);
             end
         end
-        sigrecon = sigrecon(:,dy+1:ny+dy,dx+1:nx+dx);                                           %Crop out the good data.
-        recon_tmp=fftshift(fftshift(fft(fft(ifftshift(ifftshift(sigrecon,2),3),[],2),[],3),2),3);
-        recon(:, :, :, 1, i) = flipdim(recon_tmp,2);
+        recon(:,:,:,:,t) = sigrecon(:,dy+1:ny+dy,dx+1:nx+dx);                                           %Crop out the good data.
+
     end
 
-    t=toc;
-    fprintf('... %f sec \n',t)
+    fprintf('... %f sec \n',toc)
+    
+    
+    
+    
 else
     disp('Error! The the type parameter value is wrrong!')
     recon = 0;
     ws = 0;
     return;
 end
-end
+
+
