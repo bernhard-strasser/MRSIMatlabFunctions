fa78e6565e17a44a0aa6fdbca4babc352abfa747
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2013-11-19 14:16:16 +0100

    1 Feature. 1) Feature: If weights are already known, those can be passed over to function,    and they will not be computed by the function, but only applied.

--- openslicecaipirinha_MRSI.m
+++ openslicecaipirinha_MRSI.m
@@ -1,4 +1,4 @@
-function [OutData,weights]=openslicecaipirinha_MRSI(InData, ACS, FoV_shifts, kernelsize) 
+function [OutData,weights]=openslicecaipirinha_MRSI(InData, ACS, FoV_shifts, kernelsize, weights) 
 % 
 % openslicegrappa_MRSI Reconstruct the Slices of MRSI and MRI Data Ehen Only the Sum of Those Slices Was Measured.
 % 
@@ -25,6 +25,7 @@ function [OutData,weights]=openslicecaipirinha_MRSI(InData, ACS, FoV_shifts, ker
 %                          E.g.: [1 3 5; 2 4 6]. This tells the program:
 %                          AliasedSlices(1,:) = 1 3 5 --> Slice 1 of InData contains the aliased Slices of 1,3 and 5
 %                          AliasedSlices(2,:) = 2 4 6 --> Slice 2 of Indata contains the aliased Slices of 2,4 and 6
+% -     weights            See Output. If the weights are already known, they are not computed from the ACS data.
 % 
 %   Output:
 % -     OutData            Reconstructed Output Data    (size: [#coils, nx, ny, nSlice_ACS, nTime]) (nTime = 1 for MRI)
@@ -123,119 +124,125 @@ end
 
 %% 1. Shift the ACS FoV
 
-
-ACS = kSpace_FoVShift(ACS,FoV_shifts);
+if(~exist('weights','var'))
+    ACS = kSpace_FoVShift(ACS,FoV_shifts);
+end
 
 
 
 
 %% 2. Calculate weights
 
-% Fancy Text Message
-tic;
-fprintf('\nFilling the glasses and cutting lemon slices (Calculating weights)')
 
-% What does the code do here? Let's assume ... (ohh... did I fall asleep? Hm.)
-% No, really: The source and the target points are computed for the ACS data. This is done by computing the linear indices of both
-% (to avoid slow loops). The computation of the linear indices looks quite complicated, and in fact -- it is :)
-% Well in principle the target points are just all points all x- and y-values of all channels. The relative distance of the
-% source points to a target point is computed within the kernel, and this relative information is applied to the target points
-% in order to get the source points. Then the linear indices can be computed.
-% For visualization, run the script SliceGrappa_Visualizing.m
-
-
-% Compute the Source and Target Points as linear indices
-nx_ACS_wo_border = nx_ACS - sum(kernelsize(1:2));
-ny_ACS_wo_border = ny_ACS - sum(kernelsize(3:4));
-
-kernel_Src = ones([sum(kernelsize(1:2))+1, sum(kernelsize(3:4))+1]);
-kernel_Targ = zeros([sum(kernelsize(1:2))+1, sum(kernelsize(3:4))+1]);
-kernel_Targ(kernelsize(1)+1,kernelsize(2)+1) = 1;
-
-no_SrcPts = sum(sum(sum(kernel_Src)));
-
-[SrcRelativeTarg_x,SrcRelativeTarg_y] = find(kernel_Src);
-[TargPos_x, TargPos_y] = find(kernel_Targ);
-SrcRelativeTarg = [SrcRelativeTarg_x-TargPos_x,SrcRelativeTarg_y-TargPos_y];
-clear SrcRelativeTarg_x SrcRelativeTarg_y TargPos_x TargPos_y
-
-% Source Points
-% Create channel info
-Source_Channels = int16(transpose(1:nChannel));
-Source_Channels = repmat(Source_Channels, [1 no_SrcPts nx_ACS_wo_border ny_ACS_wo_border]);
-
-% Create spatial info
-Source_x = int16(kernelsize(1)+1:nx_ACS-kernelsize(2));
-Source_y = int16(kernelsize(3)+1:ny_ACS-kernelsize(4));
-
-% Copy Source to Target Points
-Target_x = Source_x;
-Target_y = Source_y;
-
-% Apply Relative Info
-Source_x = repmat(transpose(Source_x), [1 no_SrcPts]);
-Source_y = repmat(transpose(Source_y), [1 no_SrcPts]);
-Source_x = Source_x + repmat(reshape(int16(SrcRelativeTarg(:,1)),[1 no_SrcPts]),[size(Source_x,1) 1]);
-Source_y = Source_y + repmat(reshape(int16(SrcRelativeTarg(:,2)),[1 no_SrcPts]),[size(Source_y,1) 1]);
-
-% Replicate spatial info
-Source_x = repmat(Source_x, [1 1 nChannel ny_ACS_wo_border]);
-Source_y = repmat(reshape(Source_y, [ny_ACS_wo_border no_SrcPts]), [1 1 nChannel nx_ACS_wo_border]);
-
-% Reorder Source Points
-Source_x = permute(Source_x, [3 2 1 4]);
-Source_y = permute(Source_y, [3 2 4 1]);
-
-
-% Target Points
-Target_Channels = reshape(Source_Channels(:,1,:,:), [nChannel nx_ACS_wo_border ny_ACS_wo_border]);
-Target_x = repmat(Target_x,[nChannel 1 ny_ACS_wo_border]);
-Target_y = repmat(reshape(Target_y,[1 1 numel(Target_y)]),[nChannel nx_ACS_wo_border 1]);
-
-
-% Linear Indices
-Target_linear = sub2ind( ...
-[nChannel nx_ACS ny_ACS], uint32(reshape(Target_Channels, [1 numel(Target_Channels)])), uint32(reshape(Target_x, [1 numel(Target_x)])), uint32(reshape(Target_y, [1 numel(Target_y)])));    
-Source_linear = sub2ind( ...
-[nChannel nx_ACS ny_ACS], uint32(reshape(Source_Channels, [1 numel(Source_Channels)])), uint32(reshape(Source_x, [1 numel(Source_x)])), uint32(reshape(Source_y, [1 numel(Source_y)])));
-% For Reconstructing MRSI data, uint32 has to be changed to uint64
-
-if(max(Source_linear) > 2^31)
-    max(Source_linear)
-    display('Change to uint64 in code. Aborting.')
-    OutData = InData; weights = 0;
-    return
-end
-
-
+if(~exist('weights','var'))
     
- 
-% Iterate over all Slices
-weights = cell([1 nSlice_ACS]);
-for SourceSliceIndex = 1:nSlice                                 % Iterate over the slices of the InData (so the aliased or source slices)
-    
-    % The Source Points, size: nChannel*no_SrcPts x kernel repetitions in ACS data
-    ACS_sum = sum(ACS(:,:,:,AliasedSlices(SourceSliceIndex,:)),4);
-    SourcePoints_ACS = ACS_sum(Source_linear);
-    SourcePoints_ACS = reshape(SourcePoints_ACS, [nChannel*no_SrcPts numel(SourcePoints_ACS)/(nChannel*no_SrcPts)]);     
-    
-    for TargetSliceIndex = AliasedSlices(SourceSliceIndex,:)                    % Iterate over the corresponding (corr to source slices) Target Slices.
-                                                                                % So if Slice 1 of the InData contains the aliased slices [1 3 5], then compute the weights
-        % The Target Points, size: nChannel x kernel repetitions in ACS data    % For Slices 1,3 and 5 to be able to reconstruct those out of Slice 1 of the InData.
-        ACS_sliced = ACS(:,:,:,TargetSliceIndex);    
-        TargetPoints_ACS = ACS_sliced(Target_linear);
-        TargetPoints_ACS = reshape(TargetPoints_ACS, [nChannel numel(TargetPoints_ACS)/nChannel]);        
-    
-        % find weights by fitting the source data to target data.
-        % The pinv averages over all kernel repetitions in a weighted way ('least square' solution)
-        % size: nChannel x nChannel*no_SrcPts
-        weights{TargetSliceIndex} = TargetPoints_ACS * pinv(SourcePoints_ACS); 
-    
+    % Fancy Text Message
+    tic;
+    fprintf('\nFilling the glasses and cutting lemon slices (Calculating weights)')
+
+    % What does the code do here? Let's assume ... (ohh... did I fall asleep? Hm.)
+    % No, really: The source and the target points are computed for the ACS data. This is done by computing the linear indices of both
+    % (to avoid slow loops). The computation of the linear indices looks quite complicated, and in fact -- it is :)
+    % Well in principle the target points are just all points all x- and y-values of all channels. The relative distance of the
+    % source points to a target point is computed within the kernel, and this relative information is applied to the target points
+    % in order to get the source points. Then the linear indices can be computed.
+    % For visualization, run the script SliceGrappa_Visualizing.m
+
+
+    % Compute the Source and Target Points as linear indices
+    nx_ACS_wo_border = nx_ACS - sum(kernelsize(1:2));
+    ny_ACS_wo_border = ny_ACS - sum(kernelsize(3:4));
+
+    kernel_Src = ones([sum(kernelsize(1:2))+1, sum(kernelsize(3:4))+1]);
+    kernel_Targ = zeros([sum(kernelsize(1:2))+1, sum(kernelsize(3:4))+1]);
+    kernel_Targ(kernelsize(1)+1,kernelsize(2)+1) = 1;
+
+    no_SrcPts = sum(sum(sum(kernel_Src)));
+
+    [SrcRelativeTarg_x,SrcRelativeTarg_y] = find(kernel_Src);
+    [TargPos_x, TargPos_y] = find(kernel_Targ);
+    SrcRelativeTarg = [SrcRelativeTarg_x-TargPos_x,SrcRelativeTarg_y-TargPos_y];
+    clear SrcRelativeTarg_x SrcRelativeTarg_y TargPos_x TargPos_y
+
+    % Source Points
+    % Create channel info
+    Source_Channels = int16(transpose(1:nChannel));
+    Source_Channels = repmat(Source_Channels, [1 no_SrcPts nx_ACS_wo_border ny_ACS_wo_border]);
+
+    % Create spatial info
+    Source_x = int16(kernelsize(1)+1:nx_ACS-kernelsize(2));
+    Source_y = int16(kernelsize(3)+1:ny_ACS-kernelsize(4));
+
+    % Copy Source to Target Points
+    Target_x = Source_x;
+    Target_y = Source_y;
+
+    % Apply Relative Info
+    Source_x = repmat(transpose(Source_x), [1 no_SrcPts]);
+    Source_y = repmat(transpose(Source_y), [1 no_SrcPts]);
+    Source_x = Source_x + repmat(reshape(int16(SrcRelativeTarg(:,1)),[1 no_SrcPts]),[size(Source_x,1) 1]);
+    Source_y = Source_y + repmat(reshape(int16(SrcRelativeTarg(:,2)),[1 no_SrcPts]),[size(Source_y,1) 1]);
+
+    % Replicate spatial info
+    Source_x = repmat(Source_x, [1 1 nChannel ny_ACS_wo_border]);
+    Source_y = repmat(reshape(Source_y, [ny_ACS_wo_border no_SrcPts]), [1 1 nChannel nx_ACS_wo_border]);
+
+    % Reorder Source Points
+    Source_x = permute(Source_x, [3 2 1 4]);
+    Source_y = permute(Source_y, [3 2 4 1]);
+
+
+    % Target Points
+    Target_Channels = reshape(Source_Channels(:,1,:,:), [nChannel nx_ACS_wo_border ny_ACS_wo_border]);
+    Target_x = repmat(Target_x,[nChannel 1 ny_ACS_wo_border]);
+    Target_y = repmat(reshape(Target_y,[1 1 numel(Target_y)]),[nChannel nx_ACS_wo_border 1]);
+
+
+    % Linear Indices
+    Target_linear = sub2ind( ...
+    [nChannel nx_ACS ny_ACS], uint32(reshape(Target_Channels, [1 numel(Target_Channels)])), uint32(reshape(Target_x, [1 numel(Target_x)])), uint32(reshape(Target_y, [1 numel(Target_y)])));    
+    Source_linear = sub2ind( ...
+    [nChannel nx_ACS ny_ACS], uint32(reshape(Source_Channels, [1 numel(Source_Channels)])), uint32(reshape(Source_x, [1 numel(Source_x)])), uint32(reshape(Source_y, [1 numel(Source_y)])));
+    % For Reconstructing MRSI data, uint32 has to be changed to uint64
+
+    if(max(Source_linear) > 2^31)
+        max(Source_linear)
+        display('Change to uint64 in code. Aborting.')
+        OutData = InData; weights = 0;
+        return
     end
-end
 
-fprintf('\nCutting took\t... %f sec \n',toc)                                                          
-fprintf('Aaaaahhh! This SMELL! \n') 
+
+
+
+    % Iterate over all Slices
+    weights = cell([1 nSlice_ACS]);
+    for SourceSliceIndex = 1:nSlice                                 % Iterate over the slices of the InData (so the aliased or source slices)
+
+        % The Source Points, size: nChannel*no_SrcPts x kernel repetitions in ACS data
+        ACS_sum = sum(ACS(:,:,:,AliasedSlices(SourceSliceIndex,:)),4);
+        SourcePoints_ACS = ACS_sum(Source_linear);
+        SourcePoints_ACS = reshape(SourcePoints_ACS, [nChannel*no_SrcPts numel(SourcePoints_ACS)/(nChannel*no_SrcPts)]);     
+
+        for TargetSliceIndex = AliasedSlices(SourceSliceIndex,:)                    % Iterate over the corresponding (corr to source slices) Target Slices.
+                                                                                    % So if Slice 1 of the InData contains the aliased slices [1 3 5], then compute the weights
+            % The Target Points, size: nChannel x kernel repetitions in ACS data    % For Slices 1,3 and 5 to be able to reconstruct those out of Slice 1 of the InData.
+            ACS_sliced = ACS(:,:,:,TargetSliceIndex);    
+            TargetPoints_ACS = ACS_sliced(Target_linear);
+            TargetPoints_ACS = reshape(TargetPoints_ACS, [nChannel numel(TargetPoints_ACS)/nChannel]);        
+
+            % find weights by fitting the source data to target data.
+            % The pinv averages over all kernel repetitions in a weighted way ('least square' solution)
+            % size: nChannel x nChannel*no_SrcPts
+            weights{TargetSliceIndex} = TargetPoints_ACS * pinv(SourcePoints_ACS); 
+
+        end
+    end
+
+    fprintf('\nCutting took\t... %f sec \n',toc)                                                          
+    fprintf('Aaaaahhh! This SMELL! \n')
+
+end
 
 
 
