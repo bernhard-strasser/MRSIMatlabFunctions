3a1a2265202c7aad8110ef07b7cf75ada74fae57
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2014-02-06 14:40:46 +0100

    Some Features. 1) SomeFeatures: Made basic functionality running, as NoiseDecorrelation, fft    flipping of kSpace etc.

--- PreProcessMRIData_Wrapper.m
+++ PreProcessMRIData_Wrapper.m
@@ -1,4 +1,4 @@
-function kSpace = PreProcessMRIData_Wrapper(csi_path, DesiredSize)
+function [iSpace,Noise, kSpace, PreProcessingInfo] = PreProcessMRIData_Wrapper(kSpace,PreProcessingInfo,ReadInInfo)
 %
 % read_csi_dat Read in csi-data from Siemens raw file format
 %
@@ -9,7 +9,7 @@ function kSpace = PreProcessMRIData_Wrapper(csi_path, DesiredSize)
 % some easy Postprocessing steps like zerofilling, Hadamard decoding, Noise Decorrelation etc.
 %
 %
-% [csi,NoiseCorrMat,Noise_mat,csi_kspace] = read_csi_dat(csi_path, zerofill_to_nextpow2_flag, zerofilling_fact, Hadamard_flag, x_shift,y_shift,NoFFT_flag, NoiseCorrMat)
+% [csi,NoiseCorrMat,Noise_mat,kSpace.CSI] = read_csi_dat(csi_path, zerofill_to_nextpow2_flag, zerofilling_fact, Hadamard_flag, x_shift,y_shift,NoFFT_flag, NoiseCorrMat)
 %
 % Input: 
 % -         csi_path                    ...     Path of MRS(I) file.
@@ -27,7 +27,7 @@ function kSpace = PreProcessMRIData_Wrapper(csi_path, DesiredSize)
 % -         csi                         ...     Output data in image domain. In case of Single Voxel Spectroscopy, this is the only output
 % -         NoiseCorrMat                ...     The Noise Correlation Matrix in order to check if it was properly computed from the csi data. Is 0 if no decorrelation performed.
 % -         Noise_mat                   ...     The Noise gathered from the CSI data. Noise_mat = 0, if not gathered.
-% -         csi_kspace                  ...     Output data in k-space. In case of SVS this is zero. size: channel x ROW x COL x SLC x vecSize x Averages
+% -         kSpace.CSI                  ...     Output data in k-space. In case of SVS this is zero. size: channel x ROW x COL x SLC x vecSize x Averages
 %
 %
 % Feel free to change/reuse/copy the function. 
@@ -78,182 +78,251 @@ memused_before = memused_linux_1_0(1);
 
 
 
-%% 3. Perform Noise Decorrelation
+%% 1. Loop Over All Existing DataSets In PreProcessingInfo and Preprocess Data
 
-if(exist('NoiseCorrMat','var'))
-    if(numel(NoiseCorrMat) > 1)
-    
-        fprintf('\nNoise Decorrelating Using\t...\tHanded Over NoiseCorr Matrix.')    
-        %DecorrelationMatrix = inv(chol(Noise_CorrMat/2,'lower'));
-        csi_kspace = reshape(chol(NoiseCorrMat/2,'lower') \ reshape(csi_kspace, [size(csi_kspace,1) numel(csi_kspace)/size(csi_kspace,1)]), size(csi_kspace));    % Matrix multiplication
+DataSetNames = transpose(fields(PreProcessingInfo));
+for CurDataSet = DataSetNames
+
+	CurDataSetString = CurDataSet{:};
+
+
+	% 1.1 Compute Noise Correlation Matrix if necessary
+	% Only read in data if NoiseCorrelationMatrix exists and is a 1
+	if( isfield(PreProcessingInfo.(CurDataSetString),'NoiseCorrMat') && PreProcessingInfo.(CurDataSetString).NoiseCorrMat == 1 )
+
+
+		%%%%%%% GET NOISE %%%%%%
+
+		% Get data from Noise Prescan
+		if(isfield(kSpace,'NOISE'))
+			fprintf('\nNoise Decorrelating Using\t...\tNoise Prescan.')    
+			Noise_mat = reshape(kSpace.NOISE,[size(kSpace.NOISE,1) numel(kSpace.NOISE)/size(kSpace.NOISE,1)]);
+
+		% Or from end of FID of the outer kSpace
+		elseif(isfield(kSpace,'CSI'))
+			fprintf('\nNoise Decorrelating Using\t...\tCSI Data Itself.')
+			
+			% Gather "Noise" of the CSI data (end of FIDs in the outermost acquired circle of k-space points). Only working for 2D and Multislice
+			nFIDendpoints = 400;
+			TakeNPointsOutOfEnd = 200;
+			randy = randperm(nFIDendpoints); randy = randy(1:TakeNPointsOutOfEnd); % Take 'em randomly
+			
+			% Take random points at end of FID
+			Noise_csi = kSpace.CSI(:,:,:,:,:,end - (nFIDendpoints - 1) : end); Noise_csi = Noise_csi(:,:,:,:,:,randy);
+
+			% Take only csi-points which are farest away from k-space center (so a circle with radius 31 k-space points)
+			if(ReadInInfo.General.Ascconv.Full_ElliptWeighted_Or_Weighted_Acq == 2)
+				[Elliptical_dummy,OuterkSpace_mask1] = EllipticalFilter_1_1(squeeze(kSpace.CSI(1,:,:,1,1,1,1)),[1 2],[1 1 1 size(kSpace.CSI,3)/2-1],1);
+				[Elliptical_dummy,OuterkSpace_mask2] = EllipticalFilter_1_1(squeeze(kSpace.CSI(1,:,:,1,1,1,1)),[1 2],[1 1 1 size(kSpace.CSI,3)/2-2],1);
+				OuterkSpace_mask = OuterkSpace_mask1 - OuterkSpace_mask2;
+			else
+				OuterkSpace_mask = zeros([size(kSpace.CSI,2), size(kSpace.CSI,3)]);
+				OuterkSpace_mask(1:end,1) = 1; OuterkSpace_mask(1,1:end) = 1; OuterkSpace_mask(end,1:end) = 1; OuterkSpace_mask(1:end,end) = 1;
+			end
+			PI_mask = abs(squeeze(kSpace.CSI(1,:,:,1,1,1))); PI_mask(PI_mask > 0) = 1;
+			csi_mask = OuterkSpace_mask .* PI_mask;
+			csi_mask = repmat(logical(csi_mask), [1 1 size(kSpace.CSI,1)*size(kSpace.CSI,4)*TakeNPointsOutOfEnd]);
+			csi_mask = reshape(csi_mask, [size(kSpace.CSI,2) size(kSpace.CSI,3) size(kSpace.CSI,1) size(kSpace.CSI,4) TakeNPointsOutOfEnd]);
+			csi_mask = permute(csi_mask, [3 1 2 4 5]);
+
+			Noise_mat = Noise_csi(csi_mask);
+			Noise_mat = reshape(Noise_mat, [size(kSpace.CSI,1) numel(Noise_mat)/size(kSpace.CSI,1)]);  
+		end
+
+		% Copy NoiseCorrMat
+		for CurDataSet2 = DataSetNames
+			if(strcmpi(CurDataSet2{:},'NOISE'))
+				continue;
+			end
+
+			% Rescale Noise to be the same like csi noise
+			if( isfield(ReadInInfo,'NOISE') && isfield(ReadInInfo.NOISE,'Dwelltime') && isfield(ReadInInfo,CurDataSet2{:}) && isfield(ReadInInfo.(CurDataSet2{:}),'Dwelltime') )
+				NoiseScalingFactor = sqrt(ReadInInfo.(CurDataSet2{:}).Dwelltime / ReadInInfo.NOISE.Dwelltime);
+			else
+				NoiseScalingFactor = 1;
+			end
+			Noise_mat = Noise_mat * NoiseScalingFactor;
+
+			% Compute noise correlation matrix
+			NoiseCorrMat = 1/(size(Noise_mat,2)) * (Noise_mat * Noise_mat');
+			PreProcessingInfo.(CurDataSet2{:}).NoiseCorrMat = NoiseCorrMat;
+			Noise.(CurDataSet2{:}) = Noise_mat;
+		end
+
+		% Delete Uneccessary stuff
+		clear NoisePar Noise_mat NoiseCorrMat NoiseScalingFactor;
+	end
+
+	
+	
+	
+	
+	
+	% 1.2. Perform Noise Decorrelation
+	if(isfield(PreProcessingInfo.(CurDataSetString),'NoiseCorrMat') && numel(PreProcessingInfo.(CurDataSetString).NoiseCorrMat) > 1)
+		fprintf('\nStarted')
+		NoiseCorrMat_Chol = chol(PreProcessingInfo.(CurDataSetString).NoiseCorrMat/2,'lower');
+		
+		% Perform Noise Decorrelation Slice by Slice and Part by Part to avoid extensive memory usage 
+		for SlcLoopy = 1:size(kSpace.(CurDataSetString),4)
+			for PartLoopy = 1:size(kSpae.(CurDataSetString),5)
+				
+				TempData = kSpace.(CurDataSetString)(:,:,:,SlcLoopy,PartLoopy,:,:);
+				size_TempData = size(TempData);
+				TempData = reshape(TempData, [size(TempData,1) numel(TempData)/size(TempData),1]);
+				fprintf('\nFirst Reshape')
+				TempData = NoiseCorrMat_Chol \ TempData;
+				fprintf('\nDivision')
+				kSpace.(CurDataSetString)(:,:,:,SlcLoopy,PartLoopy,:,:) = reshape(TempData,size_TempData);
+				fprintf('\nSecond Reshape')
+				
+			end
+		end
+		
+				
+				
+				
+% 		size_kSpace = size(kSpace.(CurDataSetString));
+% 		kSpace.(CurDataSetString) = reshape(kSpace.(CurDataSetString), [size(kSpace.(CurDataSetString),1) numel(kSpace.(CurDataSetString))/size(kSpace.(CurDataSetString),1)]);
+% 		fprintf('\nFirst Reshape')
+% 		NoiseCorrMat_Chol = chol(PreProcessingInfo.(CurDataSetString).NoiseCorrMat/2,'lower');
+% 		fprintf('\nchol')		
+% 		kSpace.(CurDataSetString) = NoiseCorrMat_Chol \ kSpace.(CurDataSetString);
+% 		fprintf('\nDivision')
+% 		kSpace.(CurDataSetString) = reshape(kSpace.(CurDataSetString),size_kSpace);
+% 		fprintf('\nSecond Reshape')
+
+		
+% 		kSpace.(CurDataSetString) = reshape(chol(PreProcessingInfo.(CurDataSetString).NoiseCorrMat/2,'lower') \ ...
+% 		reshape(kSpace.(CurDataSetString), [size(kSpace.(CurDataSetString),1) numel(kSpace.(CurDataSetString))/size(kSpace.(CurDataSetString),1)]), size(kSpace.(CurDataSetString)));    % Matrix multiplication
+		clear nFIDendpoints TakeNPointsOutOfEnd randy Noise_csi Elliptical_dummy OuterkSpace_mask2 OuterkSpace_mask1 OuterkSpace_mask PI_mask csi_mask
+	end
+
+
+
+	
+	% 1.3. Flip
+	if(isfield(PreProcessingInfo.(CurDataSetString),'FlipkSpaceAlong') && isfield(PreProcessingInfo.(CurDataSetString),'FlipkSpaceWhileAccessing'))
+		for FlipAlong = PreProcessingInfo.(CurDataSetString).FlipkSpaceAlong
+			eval(['kSpace.(CurDataSetString)(' PreProcessingInfo.(CurDataSetString).FlipkSpaceWhileAccessing ') = flipdim(kSpace.(CurDataSetString)(' PreProcessingInfo.(CurDataSetString).FlipkSpaceWhileAccessing ') ,FlipAlong);']);
+			%%image_kspace = circshift(image_kspace, [0 1 1 0 0]);
+		end
+	end
+
+
+
+
+
+	% 1.2 Hadamard Decoding
+	if(isfield(PreProcessingInfo.(CurDataSetString),'Hadamard_flag') && PreProcessingInfo.(CurDataSetString).Hadamard_flag && size(kSpace.(CurDataSetString),5) > 1)
+		for channel_no = 1:size(kSpace.CSI,1)
+
+			fprintf('\nHadamard decoding channel %02d\t...', channel_no)
+			tic
+
+			kSpace.(CurDataSetString)(channel_no,:,:,:,:,:,:) = hadamard_decoding(kSpace.(CurDataSetString)(channel_no,:,:,:,:,:,:),5);            
+
+			fprintf('\ttook\t%10.6f seconds', toc)              
+
+		end
+	end
+
+
+
+
+
+	% 1.3 FFT FROM K_SPACE TO DIRECT SPACE
+
+	if(isfield(PreProcessingInfo.(CurDataSetString), 'NoFFT_flag') && ~PreProcessingInfo.(CurDataSetString).NoFFT_flag)
+		if(size(kSpace.(CurDataSetString),2) > 1 || size(kSpace.(CurDataSetString),3) > 1 || size(kSpace.(CurDataSetString),4) > 1)                    % In case that MRSI data, not Single Voxel Spectroscopy data is inputted.
+
+			iSpace.(CurDataSetString) = kSpace.(CurDataSetString);
+
+			if(nargout < 3)
+				kSpace = rmfield(kSpace,CurDataSetString);
+			end    
+
+
+			% Sacher: Doing everythin at once, because enough memory is available.
+			[bla, hostname] = unix('echo $(hostname)'); clear bla;
+			if(~isempty(strfind(hostname,'sacher')))
+
+					tic_overall = tic;
+					iSpace.(CurDataSetString) = ifftshift(ifftshift(ifftshift(iSpace.(CurDataSetString),2),3),4);
+					iSpace.(CurDataSetString) = fft(fft(fft(iSpace.(CurDataSetString),[],2),[],3),[],4);
+					
+					if(strcmpi(CurDataSetString,'CSI'))
+						iSpace.(CurDataSetString) = conj(iSpace.(CurDataSetString));								 % the chem shift gets higher from right to left --> conj reverses that
+					end
+					iSpace.(CurDataSetString) = fftshift(fftshift(fftshift(iSpace.(CurDataSetString),2),3),4); 
         
+					iSpace.(CurDataSetString) = flipdim(iSpace.(CurDataSetString),2);							% THIS FLIPS LEFT AND RIGHT IN SPATIAL DOMAIN BECAUSE PHYSICIANS WANT TO SEE IMAGES FLIPPED 
+					fprintf('\nOverall FFT Process\t\t...\ttook\t%10.6f seconds', toc(tic_overall))
 
-    elseif(NoiseCorrMat)
-        fprintf('\nNoise Decorrelating Using\t...\tCSI Data Itself.')    
-        % Gather "Noise" of the CSI data (end of FIDs in the outermost acquired circle of k-space points). Only working for 2D and Multislice
-        nFIDendpoints = 400;
-        TakeNPointsOutOfEnd = 200;
-        randy = randperm(nFIDendpoints); randy = randy(1:TakeNPointsOutOfEnd); % Take 'em randomly
-        % Take random points at end of FID
-        Noise_csi = csi_kspace(:,:,:,:,end - (nFIDendpoints - 1) : end); Noise_csi = Noise_csi(:,:,:,:,randy);
+			% All others: Everything channel by channel.
+			else
 
-        % Take only csi-points which are farest away from k-space center (so a circle with radius 31 k-space points)
-        if(ParList.Full_ElliptWeighted_Or_Weighted_Acq == 2)
-            [Elliptical_dummy,OuterkSpace_mask1] = EllipticalFilter_1_1(squeeze(csi_kspace(1,:,:,1,1)),[1 2],[1 1 1 size(csi_kspace,3)/2-1],1);
-            [Elliptical_dummy,OuterkSpace_mask2] = EllipticalFilter_1_1(squeeze(csi_kspace(1,:,:,1,1)),[1 2],[1 1 1 size(csi_kspace,3)/2-2],1);
-            OuterkSpace_mask = OuterkSpace_mask1 - OuterkSpace_mask2;
-        else
-            OuterkSpace_mask = zeros([size(csi_kspace,2), size(csi_kspace,3)]);
-            OuterkSpace_mask(1:end,1) = 1; OuterkSpace_mask(1,1:end) = 1; OuterkSpace_mask(end,1:end) = 1; OuterkSpace_mask(1:end,end) = 1;
-        end
-        PI_mask = abs(squeeze(csi_kspace(1,:,:,1,1))); PI_mask(PI_mask > 0) = 1;
-        csi_mask = OuterkSpace_mask .* PI_mask;
-        csi_mask = repmat(logical(csi_mask), [1 1 size(csi_kspace,1)*size(csi_kspace,4)*TakeNPointsOutOfEnd]);
-        csi_mask = reshape(csi_mask, [size(csi_kspace,2) size(csi_kspace,3) size(csi_kspace,1) size(csi_kspace,4) TakeNPointsOutOfEnd]);
-        csi_mask = permute(csi_mask, [3 1 2 4 5]);
+				tic_overall = tic;
+				for channel_no = 1: size(iSpace.(CurDataSetString),1)
 
-        Noise_mat = Noise_csi(csi_mask);
-        Noise_mat = reshape(Noise_mat, [total_channel_no numel(Noise_mat)/total_channel_no]);  
+					tic_loop = tic;
+					fprintf('\nFouriertransforming channel %02d\t...', channel_no)
 
-        % Compute NoiseDecorrelation of CSI data
-        NoiseCorrMat = 1/(size(Noise_mat,2)) * (Noise_mat * Noise_mat');    
 
-        % Apply Noise Decorrelation
-        csi_kspace = reshape(chol(NoiseCorrMat/2,'lower') \ reshape(csi_kspace, [size(csi_kspace,1) numel(csi_kspace)/size(csi_kspace,1)]), size(csi_kspace));    % Matrix multiplication   
 
-        clear nFIDendpoints TakeNPointsOutOfEnd randy Noise_csi Elliptical_dummy OuterkSpace_mask2 OuterkSpace_mask1 OuterkSpace_mask PI_mask csi_mask
-        
-        
-    end
-    
+
+					data_channel = iSpace.(CurDataSetString)(channel_no,:,:,:,:,:);                    % This extra assignment proved to be faster than using always iSpace.(CurDataSetString)(channel_no,:,:,:,:,:). Seems that indexing is rather slow.
+
+					data_channel = ifftshift(ifftshift(ifftshift(data_channel,2),3),4);
+					data_channel = fft(fft(fft(data_channel,[],2),[],3),[],4);
+
+
+
+					data_channel = conj(data_channel);                            % the chem shift gets higher from right to left --> conj reverses that
+					data_channel = fftshift(fftshift(fftshift(data_channel,2),3),4);
+					data_channel = flipdim(data_channel,2);                       % THIS FLIPS LEFT AND RIGHT IN SPATIAL DOMAIN BECAUSE PHYSICIANS WANT TO SEE IMAGES FLIPPED
+
+					iSpace.(CurDataSetString)(channel_no,:,:,:,:,:) = data_channel; 
+
+					fprintf('\ttook\t%10.6f seconds', toc(tic_loop))       
+
+				end
+
+				clear data_channel
+				fprintf('\nOverall FFT Process\t\t...\ttook\t%10.6f seconds', toc(tic_overall))
+
+			end
+
+		else
+			if(strcmpi(CurDataSetString,'CSI'))
+				iSpace.CSI = conj(kSpace.CSI);
+				kSpace.CSI = 0;
+			end
+		end
+
+	else
+
+		iSpace.(CurDataSetString) = 0;
+
+	end
+
+
+
+
+
+% 	%% 6. Spatial Shift
+% 
+% 	if(ne(x_shift,0))
+% 		iSpace.(CurDataSetString) = circshift(iSpace.(CurDataSetString), [0 x_shift 0 0 0]);
+% 	end
+% 
+% 	if(ne(y_shift,0))
+% 		iSpace.(CurDataSetString) = circshift(iSpace.(CurDataSetString), [0 0 y_shift 0 0]);
+% 	end
+
+
+
 end
-
-
-
-
-
-
-%% 4. Hadamard Decoding
-
-if(Hadamard_flag == 1 && SLC>1 && ~ThreeD_flag)
-    for channel_no = 1:total_channel_no
-        
-        fprintf('\nHadamard decoding channel %02d\t...', channel_no)
-        tic
-
-        csi_kspace(channel_no,:,:,:,:,:) = hadamard_decoding(csi_kspace(channel_no,:,:,:,:,:));            
-     
-        fprintf('\ttook\t%10.6f seconds', toc)              
-        
-    end
-end
-
-
-
-
-
-%% 5. FFT FROM K_SPACE TO DIRECT SPACE
-
-if(~NoFFT_flag)
-    if(size(csi_kspace,2) > 1 || size(csi_kspace,3) > 1)                    % In case that MRSI data, not Single Voxel Spectroscopy data is inputted.
-
-        csi = csi_kspace;
-
-
-        if(nargout < 3)
-            clear csi_kspace;
-        end    
-
-        
-        % Sacher: Doing everythin at once, because enough memory is available.
-        [bla, hostname] = unix('echo $(hostname)'); clear bla;
-        if(~isempty(strfind(hostname,'sacher')))
- 
-                tic_overall = tic;
-                csi = ifftshift(ifftshift(csi,2),3);
-                csi = fft(fft(csi,[],2),[],3);
-                if(~Hadamard_flag && SLC > 1)                       % This is a hack for now to perform fft also in z-direction, if data has several slices but not hadamard encoded.
-                    csi = ifftshift(csi,4);                 % In the long run, this must be changed. It must be read out from the header if it is real 3D,2D or 2D+Hadamard.
-                    csi = fft(csi,[],4); 
-                end                
-                csi = conj(csi);                            % the chem shift gets higher from right to left --> conj reverses that
-                csi = fftshift(fftshift(csi,2),3); 
-                if(~Hadamard_flag && SLC > 1)                       % Same hack as above
-                    csi = fftshift(csi,4);
-                end                
-                csi = flipdim(csi,2);                       % THIS FLIPS LEFT AND RIGHT IN SPATIAL DOMAIN BECAUSE PHYSICIANS WANT TO SEE IMAGES FLIPPED 
-                fprintf('\nOverall FFT Process\t\t...\ttook\t%10.6f seconds', toc(tic_overall))
-
-        % All others: Everything channel by channel.
-        else
-          
-            tic_overall = tic;
-            for channel_no = 1: size(csi,1)
-
-                tic_loop = tic;
-                fprintf('\nFouriertransforming channel %02d\t...', channel_no)
-
-          
-
-
-                csi_channel = csi(channel_no,:,:,:,:,:);                    % This extra assignment proved to be faster than using always csi(channel_no,:,:,:,:,:). Seems that indexing is rather slow.
-
-                csi_channel = ifftshift(ifftshift(csi_channel,2),3);
-                csi_channel = fft(fft(csi_channel,[],2),[],3);
-
-                if(~Hadamard_flag && SLC > 1)                       % This is a hack for now to perform fft also in z-direction, if data has several slices but not hadamard encoded.
-                    csi_channel = ifftshift(csi_channel,4);                 % In the long run, this must be changed. It must be read out from the header if it is real 3D,2D or 2D+Hadamard.
-                    csi_channel = fft(csi_channel,[],4); 
-                end
-
-                csi_channel = conj(csi_channel);                            % the chem shift gets higher from right to left --> conj reverses that
-                csi_channel = fftshift(fftshift(csi_channel,2),3);
-                if(~Hadamard_flag && SLC > 1)                       % Same hack as above
-                    csi_channel = fftshift(csi_channel,4);
-                end
-                csi_channel = flipdim(csi_channel,2);                       % THIS FLIPS LEFT AND RIGHT IN SPATIAL DOMAIN BECAUSE PHYSICIANS WANT TO SEE IMAGES FLIPPED
-
-                csi(channel_no,:,:,:,:,:) = csi_channel; 
-
-                fprintf('\ttook\t%10.6f seconds', toc(tic_loop))       
-
-            end
-
-            clear csi_channel
-            fprintf('\nOverall FFT Process\t\t...\ttook\t%10.6f seconds', toc(tic_overall))
-        
-        end
-
-    else
-
-        csi = conj(csi_kspace);
-        csi_kspace = 0;
-
-    end
-    
-else
-    
-    csi = 0;
-    
-end
-
-
-
-
-
-%% 6. Spatial Shift
-
-if(ne(x_shift,0))
-    csi = circshift(csi, [0 x_shift 0 0 0]);
-end
-
-if(ne(y_shift,0))
-    csi = circshift(csi, [0 0 y_shift 0 0]);
-end
-
-
-
-
 
 
 
