091eab596d188b37697e563da905ef9d259a7a68
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2014-02-06 16:08:01 +0100

    Initialized Repo based on PerformNoiseDecorrelation.

---
+++ GatherNoiseFromCSI.m
@@ -0,0 +1,100 @@
+function OutData = PerformNoiseDecorrelation(InData,NoiseCorrMat)
+%
+% read_csi_dat Read in csi-data from Siemens raw file format
+%
+% This function was written by Bernhard Strasser, July 2012.
+%
+%
+% The function can read in MRS(I) data in the Siemens raw file format ".DAT" and performs
+% some easy Postprocessing steps like zerofilling, Hadamard decoding, Noise Decorrelation etc.
+%
+%
+% [csi,NoiseCorrMat,Noise_mat,kSpace.CSI] = read_csi_dat(csi_path, zerofill_to_nextpow2_flag, zerofilling_fact, Hadamard_flag, x_shift,y_shift,NoFFT_flag, NoiseCorrMat)
+%
+% Input: 
+% -         csi_path                    ...     Path of MRS(I) file.
+% -         zerofill_to_nextpow2_flag   ...     Flag, if the MRSI data should be zerofilled to the next power of 2 in k-space (e.g. 42x42 sampled --> zf to 64x64?)
+% -         zerofilling_fact            ...     Factor with which the MRSI data should be zerofilled in k-space for interpolation (e.g. zerofill from 64x64 to 128x128)
+% -         Hadamard_flag               ...     If data is multislice hadamard encoded, perform hadamard-decoding function
+% -         x_shift                     ...     Shift the MRSI data in the left-right direction ( = row direction of matrix) by x_shift voxels
+% -         y_shift                     ...     Shift the MRSI data in anterior-posterior direction ( = column direction of matrix) by y_shift voxels
+% -         NoFFT_flag                  ...     If this is true, don't perform any fft.
+% -         NoiseCorrMat                ...     If size(NoiseCorrMat) = [cha cha]: the k-space Data gets decorrelated with this matrix. 
+%                                               If NoiseCorrMat = 1: the end of the FIDs of the outermost k-space/image points are used for noise decorrelation.
+%                                               If NoiseCorrMat = 0, or not existant: No Noise Decorrelation performed
+%
+% Output:
+% -         csi                         ...     Output data in image domain. In case of Single Voxel Spectroscopy, this is the only output
+% -         NoiseCorrMat                ...     The Noise Correlation Matrix in order to check if it was properly computed from the csi data. Is 0 if no decorrelation performed.
+% -         Noise_mat                   ...     The Noise gathered from the CSI data. Noise_mat = 0, if not gathered.
+% -         kSpace.CSI                  ...     Output data in k-space. In case of SVS this is zero. size: channel x ROW x COL x SLC x vecSize x Averages
+%
+%
+% Feel free to change/reuse/copy the function. 
+% If you want to create new versions, don't degrade the options of the function, unless you think the kicked out option is totally useless.
+% Easier ways to achieve the same result & improvement of the program or the programming style are always welcome!
+% File dependancy: memused_linux_1_0,Analyze_csi_mdh_1_3, read_ascconv_1_2, hadamard_encoding_7.m
+
+% Further remarks: This function uses FFTs to get from k- to image-space. This is mathematically wrong, but Siemens seems to do the same when
+% creating DICOMS. The only difference is that the images are flipped left/right and up/down.
+
+
+
+
+
+%% 0. Preparations
+
+
+if(nargin < 2)
+	fprintf('\nProblem: Cannot Noise-Decorrelate if no Input Data or NoiseCorrMat is provided.')
+end
+
+
+%% 1. Decorrelate
+
+[dummy, memfree] = memused_linux_1_1;
+
+NoiseCorrMat_Chol = chol(NoiseCorrMat/2,'lower');
+
+
+if(numel(InData)*8*2*2/2^20 > memfree)			% every entry of InData is double --> 8 bytes. *2 because complex. *2 as safety measure (so InData fits 3 times in memory, once it is already there
+												% and two more times it should fit in). /2^20 to get from bytes to MB.
+	
+	OutData = zeros(size(InData));
+	% Perform Noise Decorrelation Slice by Slice and Part by Part to avoid extensive memory usage 
+	for SlcLoopy = 1:size(InData,4)
+		for PartLoopy = 1:size(InData,5)
+
+			TempData = InData(:,:,:,SlcLoopy,PartLoopy,:,:);
+			size_TempData = size(TempData);
+			TempData = reshape(TempData, [size(TempData,1) numel(TempData)/size(TempData),1]);
+			fprintf('\nFirst Reshape')
+			TempData = NoiseCorrMat_Chol \ TempData;
+			fprintf('\nDivision')
+			OutData(:,:,:,SlcLoopy,PartLoopy,:,:) = reshape(TempData,size_TempData);
+			fprintf('\nSecond Reshape')
+
+		end
+	end
+		
+else	
+				
+	size_kSpace = size(InData);
+	OutData = reshape(InData, [size(InData,1) numel(InData)/size(InData,1)]);
+	fprintf('\nFirst Reshape')
+	OutData = NoiseCorrMat_Chol \ OutData;
+	fprintf('\nDivision')
+	OutData = reshape(OutData,size_kSpace);
+	fprintf('\nSecond Reshape')
+
+
+% 	InData = reshape(chol(PreProcessingInfo.(CurDataSetString).NoiseCorrMat/2,'lower') \ ...
+% 	reshape(InData, [size(InData,1) numel(InData)/size(InData,1)]), size(InData));    % Matrix multiplication
+
+
+
+end
+
+
+
+
