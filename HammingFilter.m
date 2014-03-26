286ebfcd81585b41051ccad52722c1db6f14ae57
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2014-03-26 13:44:05 +0100

    Wrote function that performs fourier transforms in two dimensions, applys an 2d-hamming filter and transforms back to image domain. Based on version Hamming_filtering_1_1.

--- HammingFilter.m
+++ HammingFilter.m
@@ -1,33 +1,32 @@
-function matrix_hammingfiltered = Hamming_filtering_1_0(matrix_in,filter_along_dimension)
+function matrix_hammingfiltered = Hamming_filtering_1_1(matrix_in,do_hamming_filt_along_dimensions)
 
-% hamming_dimension = size(1,do_hamming_filt_along_dimensions);
-% do_not_hamming_filt_along_dimensions = setxor(do_hamming_filt_along_dimensions,1:numel(size(matrix)));  % setxor: computed the "complement" of two vectors, eg setxor([1 2 5], [1 2 3 4 5 6 7 8]) = [3 4 6 7 8])
-% 
-% %hamming_nD = zeros(size(matrix));
-% % convert back to k-space
-% for dimension = do_hamming_filt_along_dimensions
-%     matrix = fftshift(ifft(matrix,size(matrix,dimension),dimension));
-% end
+
+
+do_not_hamming_filt_along_dimensions = setxor(do_hamming_filt_along_dimensions,1:numel(size(matrix_in)));  % setxor: computed the "complement" of two vectors, eg setxor([2 3], [1 2 3 4 5]) = [1 4 5])
+size_mat = size(matrix_in);                                                 % size_mat(do_not...) gives the size of the matrix along the dimensions, along no hamming_filt is done prod() gives the product of all these numbers
+repmat_number = prod(size_mat(do_not_hamming_filt_along_dimensions));       % example: do_not...= [1 4 5], size_mat(do_not...) = [32 4 1024] (from matrix size(matrix_in) = [32 64 64 4 1024]); prod(...) = 32*4*1024
     
-
-
-dim1 = filter_along_dimension(1);
-dim2 = filter_along_dimension(2);
-ROW = size(matrix_in,dim1);
-COL = size(matrix_in,dim2);
-
-
-%convert back to k-space
-matrix_hammingfiltered = fftshift(fftshift(ifft(ifft(matrix_in,ROW,dim1),COL,dim2),dim1),dim2);
-
 %calculate Hamming filter
-hamming_ROW = (hamming(ROW));
-hamming_COL = (hamming(COL));
+hamming_ROW = (hamming(size(matrix_in,do_hamming_filt_along_dimensions(1))));
+hamming_COL = (hamming(size(matrix_in,do_hamming_filt_along_dimensions(2))));
 hamming2D = hamming_ROW*hamming_COL';
+hamming_repmatted = repmat(hamming2D, [1 1 repmat_number]);
+hamming_repmatted = reshape(hamming_repmatted,size(matrix_in));
+
+
+% convert back to k-space
+matrix_hammingfiltered = matrix_in;
+for dimension = do_hamming_filt_along_dimensions
+    matrix_hammingfiltered = fftshift(ifft(ifftshift(matrix_hammingfiltered,dimension),size(matrix_in,dimension),dimension),dimension);
+end
+
 
 %apply Hamming filter
-matrix_hammingfiltered=matrix_hammingfiltered.*reshape(repmat(hamming2D,1,size(matrix_hammingfiltered,4)),ROW,COL,1,size(matrix_hammingfiltered,4));
+matrix_hammingfiltered = matrix_hammingfiltered .* hamming_repmatted;
 
-%convert back to image space
-matrix_hammingfiltered = fft2(ifftshift(ifftshift(matrix_hammingfiltered,1),2),ROW,COL);
-    
+
+% convert back to image space
+for dimension = do_hamming_filt_along_dimensions
+    matrix_hammingfiltered = fftshift(fft(ifftshift(matrix_hammingfiltered,dimension),size(matrix_in,dimension),dimension),dimension);
+end
+
