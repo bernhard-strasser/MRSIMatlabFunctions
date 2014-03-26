9e0fe6d2751f27f81fdb0bf43449951bfb5052ed
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2014-03-26 13:46:05 +0100

    Implemented now the possibility to perform hamming filtering in arbitrary many and every dimension possible.

--- HammingFilter.m
+++ HammingFilter.m
@@ -1,32 +1,47 @@
-function matrix_hammingfiltered = Hamming_filtering_1_1(matrix_in,do_hamming_filt_along_dimensions)
-
-
-
-do_not_hamming_filt_along_dimensions = setxor(do_hamming_filt_along_dimensions,1:numel(size(matrix_in)));  % setxor: computed the "complement" of two vectors, eg setxor([2 3], [1 2 3 4 5]) = [1 4 5])
-size_mat = size(matrix_in);                                                 % size_mat(do_not...) gives the size of the matrix along the dimensions, along no hamming_filt is done prod() gives the product of all these numbers
-repmat_number = prod(size_mat(do_not_hamming_filt_along_dimensions));       % example: do_not...= [1 4 5], size_mat(do_not...) = [32 4 1024] (from matrix size(matrix_in) = [32 64 64 4 1024]); prod(...) = 32*4*1024
+function matrix_hammingfiltered = Hamming_filtering_1_2(matrix_in,do_hamming_filt_along_dimensions)
     
-%calculate Hamming filter
-hamming_ROW = (hamming(size(matrix_in,do_hamming_filt_along_dimensions(1))));
-hamming_COL = (hamming(size(matrix_in,do_hamming_filt_along_dimensions(2))));
-hamming2D = hamming_ROW*hamming_COL';
-hamming_repmatted = repmat(hamming2D, [1 1 repmat_number]);
-hamming_repmatted = reshape(hamming_repmatted,size(matrix_in));
 
-
-% convert back to k-space
-matrix_hammingfiltered = matrix_in;
-for dimension = do_hamming_filt_along_dimensions
-    matrix_hammingfiltered = fftshift(ifft(ifftshift(matrix_hammingfiltered,dimension),size(matrix_in,dimension),dimension),dimension);
+matrix_in_kspace = matrix_in;
+for hamming_dim = do_hamming_filt_along_dimensions
+    matrix_in_kspace = fftshift(ifft(ifftshift(matrix_in_kspace,hamming_dim),size(matrix_in,hamming_dim),hamming_dim),hamming_dim);
 end
 
 
-%apply Hamming filter
-matrix_hammingfiltered = matrix_hammingfiltered .* hamming_repmatted;
+hamming_repmatted = ones(size(matrix_in));
 
 
-% convert back to image space
-for dimension = do_hamming_filt_along_dimensions
-    matrix_hammingfiltered = fftshift(fft(ifftshift(matrix_hammingfiltered,dimension),size(matrix_in,dimension),dimension),dimension);
+
+for hamming_dim = do_hamming_filt_along_dimensions                      % Hamming filter in each dimension seperately
+    
+    
+    RepmatToSizeOfMatrixIn = size(matrix_in);                           % The Hamming-filter must have the same size as the matrix_in
+    RepmatToSizeOfMatrixIn(hamming_dim) = 1;                            % Do not repmat in that dimension, in which hamming filtering is performed.
+    
+
+
+    %calculate Hamming filter
+    hamming_1D = (hamming(size(matrix_in,hamming_dim)));
+
+    if(hamming_dim == 1)
+        hamming_1D_LeadingOnes = hamming_1D;
+    else
+        
+        reshape_to = horzcat(ones([1 hamming_dim-1]), numel(hamming_1D));
+        hamming_1D_LeadingOnes = reshape(hamming_1D,reshape_to);
+    end
+    
+    hamming_repmatted = repmat(hamming_1D_LeadingOnes, RepmatToSizeOfMatrixIn) .* hamming_repmatted;
+
+
+
 end
 
+
+
+matrix_hammingfiltered = matrix_in_kspace .* hamming_repmatted;
+
+
+
+for hamming_dim = do_hamming_filt_along_dimensions
+    matrix_hammingfiltered = fftshift(fft(ifftshift(matrix_hammingfiltered,hamming_dim),size(matrix_in,hamming_dim),hamming_dim),hamming_dim);
+end
