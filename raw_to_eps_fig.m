f0c48bc22f8c987ec77f6dd3d765d5b0ab256047
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2014-08-25 16:34:08 +0200

    Initialized repo.

---
+++ raw_to_eps_fig.m
@@ -0,0 +1,33 @@
+function Groups = template_1_0()
+%
+% 
+
+%% 0. Declarations, Preparations, Definitions
+
+% 0.1 Declarations
+
+
+% 0.2 Definitions
+    
+
+% 0.3 Preparations
+
+pause on
+
+
+
+
+%%
+
+
+
+%% 7. THE END
+
+pause off
+
+
+
+
+
+
+
