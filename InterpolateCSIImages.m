761c127bd3974f1497151b6d5272fa59ba9ef4f7
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2015-09-30 18:24:35 +0200

    Deleted unneccessary stuff. Added more function help. Changed input order. Set Mask to default if not given.

--- InterpolateCSIImages.m
+++ InterpolateCSIImages.m
@@ -1,4 +1,4 @@
-function OutArray = InterpolateCSIImages(InArray,Mask,DesiredSize,ResolutionRatio)
+function OutArray = InterpolateCSIImages(InArray,DesiredSize,Mask,ResolutionRatio)
 %
 % InterpolateCSIImages Sum or Zerofill CSI Data in Image Domain
 %
@@ -6,29 +6,28 @@ function OutArray = InterpolateCSIImages(InArray,Mask,DesiredSize,ResolutionRati
 %
 %
 % The function sums or zerofills (in kSpace) the CSI data in image domain in order to get
-% a different resolution, e.g. get the CSI from 64x64 --> 32x32
+% a different resolution, e.g. get the CSI from 64x64 --> 32x32.
 %
 %
 % [OutArray,mask] = InterpolateCSIImages(OutArray,ApplyAlongDims,EllipsoidCoefficients,PerformFFT_flag)
 %
 % Input: 
-% -     OutArray                     ...    Input array to which the filter should be applied
-% -     ApplyAlongDims               ...    Along these dimensions the filter is applied. If this vector has two elements, a two dimensional 
-%                                           Filter is applied. Otherwise, a 3d filter is used.
-% -     EllipsoidCoefficients        ...    The values for [a b c R], which determine the shape and size of the ellipsoid. For two dimensional
-%                                           Filter, set c = 1;
-% -     PerformFFT_flag              ...    If it is 0, the image gets Fourier transformed to k-space before applying the filter, 
-%                                           and transformed back to image domain afterwards
+% -     InArray                     ...     Input array which should be interpolated
+% -     DesiredSize                 ...     The desired size of the interpolated data.
+%                                           You can set it to DesiredSize = 0. Then it is derived from InArray and ResolutionRatio.
+% -     Mask                        ...     Mask with size(Mask) = size(InArray), so that we only sum spectra within the brain.
+%                                           If Mask = 0, then set Mask = true(DesiredSize), i.e. also spectra outside of the brain are summed.
+% -     ResolutionRatio             ...     The Ratio between the InArray spatial sizes and the DesiredSize. 
+%                                           You can omit it or ResolutionRatio = 0. Then it is derived from InArray and DesiredSize.
 %
 % Output:
-% -     OutArray                     ...     The filtered/masked output array
-% -     mask                         ...     The mask of the filter
+% -     OutArray                    ...     The interpolated output array
 %
 %
 % Feel free to change/reuse/copy the function. 
 % If you want to create new versions, don't degrade the options of the function, unless you think the kicked out option is totally useless.
 % Easier ways to achieve the same result & improvement of the program or the programming style are always welcome!
-% File dependancy: myrepmat_1_0
+% File dependancy: myrepmat
 
 % Further remarks: 
 
@@ -42,15 +41,19 @@ if(nargin < 1)
 	OutArray = 0;
 	return
 end
-if(nargin < 3)
+if(nargin < 2)
     return
 end
+
 size_InArray = size(InArray);
 if(numel(DesiredSize) == 1 && DesiredSize == 0)
 	DesiredSize = size_InArray(1:3) ./ ResolutionRatio;
 end
 if(~exist('ResolutionRatio','var') || (numel(ResolutionRatio) == 1 && ResolutionRatio == 0))
 	ResolutionRatio = size_InArray(1:3) ./ DesiredSize;
+end
+if(~exist('Mask','var') || (numel(Mask) == 1 && Mask == 0))
+	Mask = true(DesiredSize);
 end
 if(~islogical(Mask))
 	Mask = logical(Mask);
@@ -69,38 +72,7 @@ end
 
 
 
-
-
-
-
 %% 2. Compute Mask
-
-% OutArray = InArray;
-% for dim = 1:numel(ResolutionRatio)
-% 		
-% 		
-% 		if(ResolutionRatio(dim) == 1)
-% 			continue;
-% 		end
-% 		
-% 		InArray = zeros([size_InArray(1:dim-1) size_InArray(dim)/ResolutionRatio(dim) size_InArray(dim+1:end)]);
-% 		if(ResolutionRatio(dim) > 1)
-% 			% Sum
-% 			for XorYorZ_new = 1:size(InArray,dim)
-% 				XorYorZ_old_dwn = ResolutionRatio(dim)*(XorYorZ_new - 1) + 1;
-% 				XorYorZ_old_up = XorYorZ_new*ResolutionRatio(dim);
-% 				AssignStr = [ 'InArray(' repmat(':,',[1 dim-1]) num2str(XorYorZ_new) repmat(',:',[1 numel(size_InArray)-dim]) ') = ' ];
-% 				AssignStr = [ AssignStr 'sum(OutArray(' repmat(':,',[1 dim-1]) num2str(XorYorZ_old_dwn) ':' num2str(XorYorZ_old_up) repmat(',:',[1 numel(size_InArray)-dim]) '),' num2str(dim) ');' ];
-% 				eval(AssignStr);
-% 			end
-% 			InArray = InArray / ResolutionRatio(dim);
-% 			
-% 		elseif(ResolutionRatio(dim) < 1)
-% 			% Zerofill in kSpace
-% 		end
-% 		OutArray = InArray;
-% 		size_InArray = size(InArray);
-% end
 
 
 
