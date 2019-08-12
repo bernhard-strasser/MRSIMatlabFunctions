50328044018ffbc0db1cd47b2e3651e3094772b9
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2019-08-12 18:39:49 -0400

    SignalAnalysis/dircos: - Updated with version 1_2.

--- SignalAnalysis/dircos/compute_dircos.m
+++ SignalAnalysis/dircos/compute_dircos.m
@@ -1,4 +1,4 @@
-function [Phase_new Read_new] = compute_dircos_1_1(Slice_new,Rot_angle_around_Slice_new)
+function [Phase_new Read_new] = compute_dircos_1_2(Slice_new,Rot_angle_around_Slice_new)
 
 Phase_new = [0 Slice_new(3) -Slice_new(2)] * 1/sqrt(Slice_new(2)^2+Slice_new(3)^2);
 Read_new = -cross(Slice_new,Phase_new);
@@ -9,6 +9,9 @@ if(Rot_angle_around_Slice_new ~= 0)
     Phase_new = Rot_mat_around_Slice_new * transpose(Phase_new);    
     Read_new = Rot_mat_around_Slice_new * transpose(Read_new);
 
+    Phase_new = transpose(Phase_new);
+    Read_new = transpose(Read_new);
+    
 %     for i = 1:3
 %         Phase_new(i) = cos(Rot_angle_around_Slice_new)*Phase_new(i) - sin(Rot_angle_around_Slice_new)*Read_new(i);
 %     end
