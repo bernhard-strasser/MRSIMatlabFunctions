21cc0c0aa784c6f49dfb96ac505750f485ff79f3
bernhard-strasser <bernhard.strasser@meduniwien.ac.at>
2020-01-10 15:48:37 -0500

    1) SimulateAndShow/PlotSpectraMap/PlotSpectraMap.m: Renamed function to SimulateAndShow/plot_SpecOfAllVoxels/plot_MapOfSpectra.m and extended it. This function is based on Berkin Bilgics function to plot a map of spectra when having CSI data. 2) SimulateAndShow/plot_SpecOfAllVoxels/plot_SpecOfAllVoxels.m: Added function which plots all spectra in the same plot with different colors and the sum of all spectra. This is useful when checking whether spectral B0-corrections work or not.

---
+++ SimulateAndShow/plot_SpecOfAllVoxels/plot_MapOfSpectra.m
@@ -0,0 +1,115 @@
+function [] = plot_MapOfSpectra(InStruct, Settings, Mask)
+% InStruct.Data : input spectra
+% Settings.x_start, Settings.y_start : upper left corner of ROI
+% Settings.x_size, Settings.y_size : size of ROI to show
+% Settings.f_first, Settings.f_end : indices of first and last freqs to show, optional
+% Settings.figno_1, Settings.figno_2 : figure numbers, optional
+
+% Based on Berkin Bilgic's plot_spectra function. Modified by Bernhard Strasser. 
+
+%% Preparations
+
+
+if (~isfield(Settings,'x_start') || isempty(Settings.x_start))
+    Settings.x_start = 1;
+end
+if (~isfield(Settings,'x_size') || isempty(Settings.x_size))
+    Settings.x_size = size(InStruct.Data,1);
+end
+if (~isfield(Settings,'y_start') || isempty(Settings.y_start))
+    Settings.y_start = 1;
+end
+if (~isfield(Settings,'y_size') || isempty(Settings.y_size))
+    Settings.y_size = size(InStruct.Data,2);
+end
+if (~isfield(Settings,'f_first') || isempty(Settings.f_first))
+    Settings.f_first = 1;
+end
+if(~isfield(Settings,'f_end') || isempty(Settings.f_end))
+    Settings.f_end = size(InStruct.Data,4);
+end
+if(~isfield(Settings,'plot_z') || isempty(Settings.plot_z))
+    Settings.plot_z = floor(size(InStruct.Data,3)/2)+1;
+end
+if(~isfield(Settings,'plot_linewidth') || isempty(Settings.plot_linewidth))
+    Settings.plot_linewidth = 1;
+end
+
+OpenFigs = get(groot, 'Children');
+if(~isempty(OpenFigs)) 
+    OpenFigs = [OpenFigs(:).Number];
+else
+    OpenFigs = [];
+end
+if(~isfield(Settings,'figno_1') || isempty(Settings.Settings.figno_1))
+    Settings.figno_1 = min(setdiff(1:99,OpenFigs)); % bstr: Dont overwrite existing figures
+end
+if(~isfield(Settings,'figno_2') || isempty(Settings.Settings.figno_2))
+    OpenFigs = [OpenFigs Settings.figno_1];
+    Settings.figno_2 = min(setdiff(1:99,OpenFigs)); % bstr: Dont overwrite existing figures
+end
+
+if(isfield(Settings,'UseThisInStructMask'))
+    Mask = InStruct.(Settings.UseThisInStructMask);
+end
+if(exist('Mask','var') && ~isempty(Mask))
+    InStruct.Data = InStruct.Data .* Mask;
+    [Settings.x_start,Settings.y_start] = find(Mask);
+    Settings.x_end = max(Settings.x_start); Settings.y_end = max(Settings.y_start);
+    Settings.x_start = min(Settings.x_start); Settings.y_start = min(Settings.y_start);
+    Settings.x_size = Settings.x_end - Settings.x_start + 1; Settings.y_size = Settings.y_end - Settings.y_start + 1;
+end
+
+
+%%
+Data_fft = fftshift(fft(InStruct.Data,[],4),4);
+Data_ = abs(Data_fft(Settings.x_start:Settings.x_start+Settings.x_size-1, Settings.y_start:Settings.y_start+Settings.y_size-1,Settings.plot_z,Settings.f_first:Settings.f_end));
+y_max = max(Data_(:));
+
+
+pixels = size(Data_fft,2) * size(Data_fft,1);
+nx = sqrt(pixels);
+
+
+nnx = Settings.x_size;
+nny = Settings.y_size;
+
+widthx = 1/nnx*.9;
+widthy = 1/nny*.9;
+marginx = 1/nnx*0.05;
+marginy = 1/nny*0.05;
+originx = marginx:1/nnx:1;
+originy = 1-1/nny+marginy:-1/nny:0;
+
+
+xorig = Settings.x_start;
+yorig = Settings.y_start;
+
+
+x = xorig:xorig+nnx-1;
+y = yorig:yorig+nny-1;
+
+
+figure(Settings.figno_1);
+for i=1:nnx
+    for j=1:nny
+%        subplot('Position', [originx(i), originy(j), widthx, widthy])
+        subplot('Position', [originy(nny-j+1), originx(nnx-i+1), widthy, widthx])
+        
+        plot(Settings.f_first:Settings.f_end, abs((squeeze(Data_fft(x(i),y(j),Settings.plot_z, Settings.f_first:Settings.f_end)))),'k', 'LineWidth', Settings.plot_linewidth); 
+        axis([Settings.f_first,Settings.f_end, 0, y_max/1]); axis off;
+
+    end
+end
+
+
+ref_image = sum(abs(Data_fft(:,:,Settings.plot_z,Settings.f_first:Settings.f_end)),4);
+ref_image = ref_image / max(abs(ref_image(:)));
+
+% dB scale
+% ref_image = 20*log10(sum(abs(Data_fft(:,:,Settings.f_first:Settings.f_end)),3));
+
+figure(Settings.figno_2); imagesc(abs(ref_image)); colormap jet; colorbar; axis square; hold on;
+rectangle('Position',[y(1),x(1),y(end)-y(1),x(end)-x(1)],'EdgeColor','y', 'LineStyle','--');
+hold off;
+
---
+++ SimulateAndShow/plot_SpecOfAllVoxels/plot_SpecOfAllVoxels.m
@@ -0,0 +1,58 @@
+function plot_SpecOfAllVoxels(MRStruct, Settings,Mask)
+%
+% op_PermuteMRData Permute MR Data
+%
+% This function was written by Bernhard Strasser, Oct 2019.
+%
+%
+% The function just runs the "permute" function on MR data as specified by Settings.PermuteVec.
+% It also takes care of things like updating the .Par.DataSize, and permuting the NoiseData, if available.
+%
+%
+% [MRStruct] = op_PermuteMRData(MRStruct,Settings)
+%
+% Input: 
+% -         MRStruct               ...      The structure containing the MR data. Can have different fields. For this function, it
+%                                           it has to have field
+%                                           * .Data
+% -         Settings               ...      Structure with settings how data should be processed. Should have field:
+%                                           * .PermuteVec: Data will be permuted to according to this vector.
+%
+% Output:
+% -         MRStruct               ...      The structure containing the MR data. Can have different fields.
+%
+%
+% Feel free to change/reuse/copy the function. 
+% If you want to create new versions, don't degrade the options of the function, unless you think the kicked out option is totally useless.
+% Easier ways to achieve the same result & improvement of the program or the programming style are always welcome!
+% File dependancy: memused_linux,Analyze_csi_mdh_1_3, read_ascconv_1_2, hadamard_encoding_7.m
+
+% Further remarks: 
+
+%% Preparations
+
+if(~exist('Settings','var'))
+    Settings = struct;
+end
+if(~isfield(Settings,'UseThisInStructMask'))
+    Settings.UseThisInStructMask = 'BrainMask';
+end
+if(isfield(Settings,'UseThisInStructMask') && ~exist('Mask','var'))
+    Mask = MRStruct.(Settings.UseThisInStructMask);
+end
+if(~exist('Mask','var'))
+    Mask = ones(size_MultiDims(MRStruct.Data,1:3));
+end
+
+%% 
+
+MRStruct.Data = MRStruct.Data(:,:,:,:,1);
+MRStruct.Data = MRStruct.Data .* Mask;
+MRStruct.Data = abs(fftshift(fft(MRStruct.Data,[],4),4));
+MRStruct.Data = permute(MRStruct.Data,[4 1 2 3]);
+chemy = compute_chemshift_vector(MRStruct.RecoPar);
+figure; plot(chemy,MRStruct.Data(:,:))
+figure; plot(chemy,sum(MRStruct.Data(:,:),2))
+
+
+
